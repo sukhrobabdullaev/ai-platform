54bc221d646b893779e182afecbbc1c9bb2c51e8
sukhrobabdullaev <sukhrobjonabdullaev3365@gmail.com>
2024-01-31 15:21:59 +0500

    image generation

---
+++ app/api/image/route.ts
@@ -0,0 +1,61 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+// import { Configuration, OpenAIApi } from "openai";
+
+// import { checkSubscription } from "@/lib/subscription";
+// import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
+
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  organization: "org-Mw3S7KWPFGg8VWQ9KpZ7Dkph",
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { prompt, amount = 1, resolution = "512x512" } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    // if (!configuration.apiKey) {
+    //   return new NextResponse("OpenAI API Key not configured.", {
+    //     status: 500,
+    //   });
+    // }
+
+    if (!prompt) {
+      return new NextResponse("Prompt is required", { status: 400 });
+    }
+    if (!amount) {
+      return new NextResponse("Amount is required", { status: 400 });
+    }
+    if (!resolution) {
+      return new NextResponse("Resolution is required", { status: 400 });
+    }
+
+    // const freeTrial = await checkApiLimit();
+    // const isPro = await checkSubscription();
+
+    // if (!freeTrial && !isPro) {
+    //   return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
+    // }
+
+    // if (!isPro) {
+    //   await incrementApiLimit();
+    // }
+    const response = await openai.createImage({
+      prompt,
+      n: parseInt(amount, 10),
+      size: resolution,
+    });
+
+    return NextResponse.json(response.data.data);
+  } catch (error) {
+    console.log("[IMAGE_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+import { hostname } from "os";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [{ hostname: "oaidalleapiprodscus.blob.core.window.net" }],
+  },
+};
 
 export default nextConfig;
